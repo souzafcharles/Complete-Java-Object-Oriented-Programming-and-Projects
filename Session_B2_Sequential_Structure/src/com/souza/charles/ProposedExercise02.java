e853c96037735739b5578bd5ccfcffc9f44f7ad1
Charles Fernandes de Souza <souzach@outlook.com>
2024-03-21 18:11:06 -0300

    Update Proposed Exercise 02

--- Session_B2_Sequential_Structure/src/com/souza/charles/ProposedExercise02.java
+++ Session_B2_Sequential_Structure/src/com/souza/charles/ProposedExercise02.java
@@ -9,11 +9,9 @@ Date: March 21, 2024
 /**
 Exercise 02: Write a program to read the value of the radius of a circle,
 and then display the value of the area of this circle to four decimal places.
-
 Area formula: area = π . radius^2 
 Consider the value of π = 3.14159
 */
-
 import java.util.Scanner;
 
 public class ProposedExercise02 {
@@ -26,7 +24,7 @@ public class ProposedExercise02 {
 		double area = pi * Math.pow(radius, 2);
 		System.out.printf("Area: %.4f", area);
 		scanner.close();
+		
 	}
-
 	
 }
