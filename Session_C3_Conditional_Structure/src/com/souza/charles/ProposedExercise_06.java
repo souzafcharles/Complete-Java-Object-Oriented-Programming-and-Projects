5a696090d2b44b9b8ebc52fa479f1eafa437f185
Charles Fernandes de Souza <souzach@outlook.com>
2024-03-25 22:48:14 -0300

    Update Proposed Exercise 06

--- Session_C3_Conditional_Structure/src/com/souza/charles/ProposedExercise_06.java
+++ Session_C3_Conditional_Structure/src/com/souza/charles/ProposedExercise_06.java
@@ -9,7 +9,7 @@ Date: March 25, 2024
 /**
 Exercise 06: You are required to create a program that reads any value 
 and presents a message stating in which of the following intervals 
-([0,25], (25,50], (50,75], (75,100]) this value is located. 
+([0,25], [25,50], [50,75], [75,100]) this value is located. 
 Clearly, if the value is not within any of these intervals, 
 the message 'Out of range' should be printed.
 */
@@ -24,11 +24,11 @@ public class ProposedExercise_06 {
 		if (value >= 0 && value <= 25) {
 			System.out.println("Interval [0,25]");
 		} else if (value >= 25 && value <= 50) {
-			System.out.println("Interval (25,50]");
+			System.out.println("Interval [25,50]");
 		} else if (value >= 50 && value <= 75) {
-			System.out.println("Interval (50,75]");
+			System.out.println("Interval [50,75]");
 		} else if (value >= 75 && value <= 100) {
-			System.out.println("Interval (75,100]");
+			System.out.println("Interval [75,100]");
 		} else {
 			System.out.println("Out of range");
 		}
