2f947777a773a054a4bafce3599d811bf29a888c
Charles Fernandes de Souza <souzach@outlook.com>
2024-03-26 18:14:38 -0300

    Update Proposed Exercise 07

--- Session_C3_Conditional_Structure/src/com/souza/charles/ProposedExercise_07.java
+++ Session_C3_Conditional_Structure/src/com/souza/charles/ProposedExercise_07.java
@@ -4,7 +4,7 @@ package com.souza.charles;
 Course title: Complete Java - Object-Oriented Programming + Projects
 Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
 Exercise done by: Charles Fernandes de Souza
-Date: March 25, 2024
+Date: March 26, 2024
 */
 /**
 Exercise 07: Read 2 values with one decimal place (x and y), 
