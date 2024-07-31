ea2228d6a6c46103368202b762aa09a0b61ebf00
souzafcharles <souzach@outlook.com>
2024-07-30 23:48:35 -0300

    update ExampleExerciseProblem_01

--- Session_D4_Repetitive_Structure/src/com/souza/charles/ExampleExerciseProblem_01.java
+++ Session_D4_Repetitive_Structure/src/com/souza/charles/ExampleExerciseProblem_01.java
@@ -24,20 +24,24 @@ import java.util.Scanner;
 
 public class ExampleExerciseProblem_01 {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		int sum = 0;
-		while (true) {
-			int number = scanner.nextInt();
-			if (number == 0) {
-				break;
-			}
-			sum += number;
-		}
-		System.out.println(sum);
-		scanner.close();
+    Scanner input = new Scanner(System.in);
 
-	}
+    int sum = 0;
+
+    while (true) {
+      int number = input.nextInt();
+      if (number == 0) {
+        break;
+      }
+      sum += number;
+    }
+
+    System.out.println(sum);
+
+    input.close();
+
+  }
 
 }
