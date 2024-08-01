e80a142f5b8f4f0942a0ba9111d70d02fbcc6faa
Charles Fernandes de Souza <souzach@outlook.com>
2024-07-31 22:58:00 -0300

    Update Example With For Loop 02

--- Session_D4_Repetitive_Structure/src/com/souza/charles/ExampleWithForLoop_02.java
+++ Session_D4_Repetitive_Structure/src/com/souza/charles/ExampleWithForLoop_02.java
@@ -6,9 +6,7 @@ Example adapted by: Charles Fernandes de Souza
 Date: July 31, 2024
 */
 public class ExampleWithForLoop_02 {
-	
 	public static void main(String[] args) {
-		
 		for (int i = 4; i >=0; i--) {
 			System.out.println("i value: " + i);
 		}	
