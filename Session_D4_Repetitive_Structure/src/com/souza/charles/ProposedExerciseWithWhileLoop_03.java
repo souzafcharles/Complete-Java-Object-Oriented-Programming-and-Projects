99ad2109c03ffaceb907351d0ef2394a9acf3e96
Charles Fernandes de Souza <souzach@outlook.com>
2024-11-04 13:50:29 -0300

    Session Repetitive Structure

--- Session_D4_Repetitive_Structure/src/com/souza/charles/ProposedExerciseWithWhileLoop_03.java
+++ Session_D4_Repetitive_Structure/src/com/souza/charles/ProposedExerciseWithWhileLoop_03.java
@@ -9,10 +9,10 @@ Date: July 31, 2024
 /**
 Exercise 03: A petrol station wishes to determine which of its products is 
 preferred by its customers. Write an algorithm to read the type of fuel supplied 
-(coded as follows: 1. Alcohol 2. Petrol 3. Diesel 4. End). If the user enters an 
+(coded as follows: 1. Alcohol 2. Gasoline 3. Diesel 4. End). If the user enters an 
 invalid code (outside the range of 1 to 4), a new code should be requested 
 (until it is valid). The program will terminate when the code entered is the number 4. 
-The message "THANK YOU VERY MUCH" and the quantity of customers who filled up each 
+The message "THANK YOU" and the quantity of customers who filled up each 
 type of fuel should be written, as in the example:
 
 Examples:
@@ -21,42 +21,35 @@ Input         Output
 _______________________
   8         THANK YOU         
   1         Alcohol: 1         
-  7          Petrol: 2        
+  7          Gasoline: 2        
   2          Diesel: 0        
   2              
   4       
 _______________________
 */
-
 import java.util.Scanner;
 
 public class ProposedExerciseWithWhileLoop_03 {
-
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
-    int alcohol = 0;
-    int petrol = 0;
-    int diesel = 0;
-
+    int contAlcohol = 0;
+    int contGasoline = 0;
+    int contDiesel = 0;
     while (true) {
       int typeOfFuel = scanner.nextInt();
       if (typeOfFuel == 4) {
         break;
       } else if (typeOfFuel == 1) {
-        alcohol += 1;
+        contAlcohol++;
       } else if (typeOfFuel == 2) {
-        petrol += 1;
+        contGasoline++;
       } else if (typeOfFuel == 3) {
-        diesel += 1;
+        contDiesel++;
       } else {
-        System.out.println("Type a valid code: 1. Alcohol; 2. Petrol; 3. Diesel; 4. End");
+        System.out.println("Type a valid code: 1. Alcohol; 2. Gasoline; 3. Diesel; 4. End");
       }
     }
-    System.out.println(String.format("THANK YOU\nAlcohol: %2d\nPetrol: %2d\nDiesel: %2d", alcohol, petrol, diesel));
-
+    System.out.printf("THANK YOU\nAlcohol: %2d\nGasoline: %2d\nDiesel: %2d%n", contAlcohol++, contGasoline++, contDiesel++);
     scanner.close();
-
   }
-
 }
