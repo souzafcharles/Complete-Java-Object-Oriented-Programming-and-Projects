8a172ca5378505e67d6dcfd903b6618bca1ace81
souzafcharles <souzach@outlook.com>
2024-11-05 11:53:08 -0300

    Session Introduction to Object Oriented Programming

--- Session_F6_Introduction_to_Object_Oriented_Programming/Example02/src/com/souza/charles/application/App.java
+++ Session_F6_Introduction_to_Object_Oriented_Programming/Example02/src/com/souza/charles/application/App.java
@@ -1,5 +1,14 @@
 package com.souza.charles.application;
 
+import java.util.Scanner;
+
 public class App {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in); 
+		
+		scanner.close();
+	}
 
 }
