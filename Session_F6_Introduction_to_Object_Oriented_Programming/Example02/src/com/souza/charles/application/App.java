85e0567e596981aa2d3cd5393e0967c409402bf7
souzafcharles <souzach@outlook.com>
2024-11-05 12:24:21 -0300

    Session Introduction to Object Oriented Programming

--- Session_F6_Introduction_to_Object_Oriented_Programming/Example02/src/com/souza/charles/application/App.java
+++ Session_F6_Introduction_to_Object_Oriented_Programming/Example02/src/com/souza/charles/application/App.java
@@ -1,14 +1,21 @@
 package com.souza.charles.application;
 
 import java.util.Scanner;
+import com.souza.charles.entities.Triangle;
+import static com.souza.charles.entities.Triangle.calculateAreaTriangle;
 
 public class App {
-	
-	public static void main (String[] args) {
-		
-		Scanner scanner = new Scanner(System.in); 
-		
-		scanner.close();
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        double sideA = scanner.nextDouble();
+        double sideB = scanner.nextDouble();
+        double sideC = scanner.nextDouble();
+
+        Triangle triangleX = new Triangle(sideA, sideB, sideC);
+        System.out.printf("Triangle X area: %.4f\n", calculateAreaTriangle (sideA, sideB, sideC));
+
+        scanner.close();
+    }
 }
+
--- Session_F6_Introduction_to_Object_Oriented_Programming/Example02/src/com/souza/charles/entities/Triangle.java
+++ Session_F6_Introduction_to_Object_Oriented_Programming/Example02/src/com/souza/charles/entities/Triangle.java
@@ -2,4 +2,18 @@ package com.souza.charles.entities;
 
 public class Triangle {
 
+    double sideA, sideB, sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public static Double calculateAreaTriangle (double sideA, double sideB, double sideC){
+        double  perimeter = (sideA + sideB + sideC) / 2.0;
+        return  Math.sqrt(perimeter * (perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC));
+    }
+
 }
+
