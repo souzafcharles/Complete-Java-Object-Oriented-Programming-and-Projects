d5f48430f89bd655c6b5c4eb5611b4d406431a24
souzafcharles <souzach@outlook.com>
2024-11-05 16:23:52 -0300

    Session Introduction to Object Oriented Programming

--- Session_F6_Introduction_to_Object_Oriented_Programming/Example04/src/com/souza/charles/entities/Product.java
+++ Session_F6_Introduction_to_Object_Oriented_Programming/Example04/src/com/souza/charles/entities/Product.java
@@ -1,5 +1,10 @@
 package com.souza.charles.entities;
-
+/**
+ * Course title: Complete Java - Object-Oriented Programming + Projects
+ * Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ * Exercise done by: Charles Fernandes de Souza
+ * Date: November 05, 2024
+ */
 public class Product {
 
     public String name;
