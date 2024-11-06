720404b30c326cabdcc333584eb9dce60decf9bc
Charles Fernandes de Souza <souzach@outlook.com>
2024-11-06 15:47:10 -0300

    Session Introduction to Object Oriented Programming

--- Session_F6_Introduction_to_Object_Oriented_Programming/Exercise01/src/com/souza/charles/application/App.java
+++ Session_F6_Introduction_to_Object_Oriented_Programming/Exercise01/src/com/souza/charles/application/App.java
@@ -5,30 +5,6 @@ package com.souza.charles.application;
  * Exercise done by: Charles Fernandes de Souza
  * Date: November 05, 2024
  */
-/**
- Exercise 01: Write a program to read the values of the width and height of a rectangle.
- Then, display on the screen the value of its area, perimeter, and diagonal.
- Use a class as shown in the project below.
-
- Rectangle
- _____________________
- - Width: double
- - Height: double
- _____________________
- + Area(): double
- + Perimeter(): double
- + Diagonal(): double
-
- Examples:
- __________________________________________________________
- Input                                               Output
- __________________________________________________________
- Enter rectangle width and height:             AREA = 12.00
- 3.00                                     PERIMETER = 14.00
- 4.00                                       DIAGONAL = 5.00
- __________________________________________________________
- */
-
 import com.souza.charles.entities.Rectangle;
 import java.util.Scanner;
 
