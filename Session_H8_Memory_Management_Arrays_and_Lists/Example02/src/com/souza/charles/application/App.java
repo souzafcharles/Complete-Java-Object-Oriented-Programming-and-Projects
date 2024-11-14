c8aea0479aaec450f8a71793c55d07ef5e22f48b
Charles Fernandes de Souza <souzach@outlook.com>
2024-11-14 19:01:44 -0300

    Header reference

--- Session_H8_Memory_Management_Arrays_and_Lists/Example02/src/com/souza/charles/application/App.java
+++ Session_H8_Memory_Management_Arrays_and_Lists/Example02/src/com/souza/charles/application/App.java
@@ -1,5 +1,10 @@
 package com.souza.charles.application;
-
+/**
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Example adapted by: Charles Fernandes de Souza
+ Date: November 14, 2024
+ */
 import com.souza.charles.entities.Product;
 import java.util.Scanner;
 
@@ -8,17 +13,14 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         Product[] arrayProduct = new Product[N];
-
         for (int i = 0; i < arrayProduct.length; i++) {
             scanner.nextLine();
             String name = scanner.nextLine();
             double price = scanner.nextDouble();
             arrayProduct[i] = new Product(name, price);
         }
-
         double averagePriceOfProductsResults = Product.averagePriceOfProducts(arrayProduct);
         System.out.printf("AVERAGE PRICE = %.2f%n", averagePriceOfProductsResults);
         scanner.close();
     }
 }
-
