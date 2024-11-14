099a1fa827cb377bffbfd64335cb206f1eecdf90
Charles Fernandes de Souza <souzach@outlook.com>
2024-11-14 19:02:59 -0300

    Header reference

--- Session_H8_Memory_Management_Arrays_and_Lists/Example02/src/com/souza/charles/entities/Product.java
+++ Session_H8_Memory_Management_Arrays_and_Lists/Example02/src/com/souza/charles/entities/Product.java
@@ -1,5 +1,10 @@
 package com.souza.charles.entities;
-
+/**
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Example adapted by: Charles Fernandes de Souza
+ Date: November 14, 2024
+ */
 public class Product {
 
     private String name;
