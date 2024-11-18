634edbb5888ba2264f190c029051d6799bf53d5e
souzafcharles <souzach@outlook.com>
2024-11-17 21:10:35 -0300

    Boxing, Unboxing and Wrapper Classes

---
+++ Session_H8_Memory_Management_Arrays_and_Lists/Example03/src/com/souza/charles/Example03.java
@@ -0,0 +1,17 @@
+package com.souza.charles;
+/**
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Example adapted by: Charles Fernandes de Souza
+ Date:  November 17, 2024
+ */
+public class Example03 {
+    public static void main(String[] args) {
+        int number = 20;
+        Integer object = number;
+        System.out.println(number);
+        int otherNumber = object * 2;
+        System.out.println(otherNumber);
+
+    }
+}
