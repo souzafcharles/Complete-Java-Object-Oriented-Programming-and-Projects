962755ec2627ad5190ca66f37f345e9b1bba09a3
souzafcharles <souzach@outlook.com>
2024-11-25 09:12:38 -0300

    Include test example from Example 01

--- Session_K11_Inheritance_and_Polymorphism/Example01/src/com/souza/charles/view/App.java
+++ Session_K11_Inheritance_and_Polymorphism/Example01/src/com/souza/charles/view/App.java
@@ -6,8 +6,10 @@ public class App {
 
     public static void main(String[] args) {
 
-        BusinessAccount businessAccount = new BusinessAccount();
-
-
+        BusinessAccount businessAccount = new BusinessAccount(1080, "Guerino Crispim", 0.0, 500.0);
+        System.out.println(businessAccount.getNumber());
+        System.out.println(businessAccount.getHolder());
+        System.out.println(businessAccount.getBalance());
+        System.out.println(businessAccount.getLoanLimit());
     }
 }
