ce8d05cb58204b968f276278a703e10c7617a625
souzafcharles <souzach@outlook.com>
2024-11-28 15:25:16 -0300

    Solution Example01 | Session Handling Exceptions

---
+++ Session_L12_Handling_Exceptions/Example01/src/com/souza/charles/view/application/App.java
@@ -0,0 +1,29 @@
+package com.souza.charles.view.application;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Example adapted by: Charles Fernandes de Souza
+  Date: November28, 2024
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] vector = scanner.nextLine().split(" ");
+            int position = scanner.nextInt();
+            System.out.println(vector[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+        } catch (InputMismatchException e) {
+            System.out.println("Input error!");
+        }
+        System.out.println("End of program!");
+        scanner.close();
+    }
+}
