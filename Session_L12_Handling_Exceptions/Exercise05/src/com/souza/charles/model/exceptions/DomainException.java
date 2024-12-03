8a5f22f27c29aeb5a62269085ec37ace1a665494
souzafcharles <souzach@outlook.com>
2024-12-03 14:32:20 -0300

    Header reference Exercise 05 | Session Handling Exceptions

--- Session_L12_Handling_Exceptions/Exercise05/src/com/souza/charles/model/exceptions/DomainException.java
+++ Session_L12_Handling_Exceptions/Exercise05/src/com/souza/charles/model/exceptions/DomainException.java
@@ -1,4 +1,10 @@
 package com.souza.charles.model.exceptions;
+/*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Exercise done by: Charles Fernandes de Souza
+  Date: December 03, 2024
+ */
 
 public class DomainException extends RuntimeException {
 
--- Session_L12_Handling_Exceptions/Exercise05/src/com/souza/charles/view/application/App.java
+++ Session_L12_Handling_Exceptions/Exercise05/src/com/souza/charles/view/application/App.java
@@ -1,4 +1,10 @@
 package com.souza.charles.view.application;
+/*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Exercise done by: Charles Fernandes de Souza
+  Date: December 03, 2024
+ */
 
 import com.souza.charles.model.entities.Account;
 import com.souza.charles.model.exceptions.DomainException;
