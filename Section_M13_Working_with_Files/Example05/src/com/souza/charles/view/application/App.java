7d28477e16a2c38b5d7c1e6a43bcc6bc3d57eece
souzafcharles <souzach@outlook.com>
2024-12-04 12:53:24 -0300

    Example 05 | Working with Files

---
+++ Section_M13_Working_with_Files/Example05/src/com/souza/charles/view/application/App.java
@@ -0,0 +1,28 @@
+package com.souza.charles.view.application;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a folder path: ");
+        String strPath = scanner.nextLine();
+        File path = new File(strPath);
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS:");
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES:");
+        for (File file : files) {
+            System.out.println(file);
+        }
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
+        scanner.close();
+    }
+}
