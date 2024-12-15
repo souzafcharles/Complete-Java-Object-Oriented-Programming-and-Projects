84d7faea571ee5731f5da35b32cbb48b883f232d
souzafcharles <souzach@outlook.com>
2024-12-15 20:32:02 -0300

    Problem Statement Example 03 | Interfaces

---
+++ Section_N14_Interfaces/ProblemStatementExample03/src/com/souza/charles/model/entities/Shape.java
@@ -0,0 +1,25 @@
+package com.souza.charles.model.entities;
+
+import com.souza.charles.model.enums.Color;
+
+public abstract class Shape {
+
+    private Color color;
+
+    public Shape() {
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area ();
+}
