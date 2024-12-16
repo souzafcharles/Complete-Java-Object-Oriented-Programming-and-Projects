f44fed27cb8cab8068ef2ba00f9ba00a6b2474ca
souzafcharles <souzach@outlook.com>
2024-12-16 15:58:11 -0300

    Problem Statement Example 07 | Interfaces

--- Section_N14_Interfaces/ProblemStatementExample07/src/com/souza/charles/view/application/App.java
+++ Section_N14_Interfaces/ProblemStatementExample07/src/com/souza/charles/view/application/App.java
@@ -1,21 +1,36 @@
 package com.souza.charles.view.application;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Example adapted by: Charles Fernandes de Souza
+  Date: December 16, 2024
+ */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class App {
 
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
         String path = "C:\\tmp\\in.txt";
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
-            String line = bufferedReader.readLine();
-            while ( line != null){
-                System.out.println(line);
-                line = bufferedReader.readLine();
+            String name = bufferedReader.readLine();
+            while (name != null) {
+                list.add(name);
+                name = bufferedReader.readLine();
             }
-        }
-        catch(IOException e) {
+            Collections.sort(list);
+            for (String s : list) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
