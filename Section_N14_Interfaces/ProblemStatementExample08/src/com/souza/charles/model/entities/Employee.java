2eaceaffe9f3c5764cf34e8c00ca101880fb74ee
souzafcharles <souzach@outlook.com>
2024-12-16 16:53:00 -0300

    Solutiopn Problem Statement Example 08 | Interfaces

--- Section_N14_Interfaces/ProblemStatementExample08/src/com/souza/charles/model/entities/Employee.java
+++ Section_N14_Interfaces/ProblemStatementExample08/src/com/souza/charles/model/entities/Employee.java
@@ -1,6 +1,12 @@
 package com.souza.charles.model.entities;
+/*
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Example adapted by: Charles Fernandes de Souza
+ Date: December 16, 2024
+*/
 
-public class Employee {
+public class Employee implements Comparable<Employee> {
     private String name;
     private Double salary;
 
@@ -26,5 +32,11 @@ public class Employee {
 
     public void setSalary(Double salary) {
         this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(Employee other) {
+        return name.compareTo(other.getName());
+        //return salary.compareTo(other.getSalary());
     }
 }
--- Section_N14_Interfaces/ProblemStatementExample08/src/com/souza/charles/view/application/App.java
+++ Section_N14_Interfaces/ProblemStatementExample08/src/com/souza/charles/view/application/App.java
@@ -1,4 +1,38 @@
 package com.souza.charles.view.application;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Example adapted by: Charles Fernandes de Souza
+  Date: December 16, 2024
+ */
+
+import com.souza.charles.model.entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
+
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        String path = "C:\\tmp\\in.txt";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String employeesFile = bufferedReader.readLine();
+            while (employeesFile != null) {
+                String[] fields = employeesFile.split(",");
+                employees.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+                employeesFile = bufferedReader.readLine();
+            }
+            Collections.sort(employees);
+            for (Employee employee : employees) {
+                System.out.printf("%s, %.2f%n", employee.getName(), employee.getSalary());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
