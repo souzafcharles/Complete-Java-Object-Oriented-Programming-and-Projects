b008666fe7544a7c149928767def389334fe8ab6
souzafcharles <souzach@outlook.com>
2024-12-18 18:23:02 -0300

    Project set up | Example 03 | Section Generics, Set and Map

---
+++ Section_O15_Generics_Set_and_Map/ProblemStatementExample03/README.md
@@ -0,0 +1,19 @@
+# Problem Statement Example 02
+
+## Motivating Problem 02 (Type Safety & Performance)
+
+**Instructions**: Write a program that reads an integer N, followed by N integer numbers. Print the numbers in a
+formatted list as shown in the example. Finally, output the first number entered.
+
+### PrintService Model
+
+![Print Service Model](https://github.com/souzafcharles/Complete-Java-Object-Oriented-Programming-and-Projects/blob/master/Section_O15_Generics_Set_and_Map/ProblemStatementExample02/print-service-model.png)
+
+### Example Inputs and Outputs
+
+| **Input**            | **Output**  |
+|----------------------|-------------|
+| `How many values?` 3 | [10, 8, 23] |
+| 10                   | `First`: 10 |
+| 8                    |             |
+| 23                   |             |
---
+++ Section_O15_Generics_Set_and_Map/ProblemStatementExample03/src/com/souza/charles/model/services/PrintService.java
@@ -0,0 +1,37 @@
+package com.souza.charles.model.services;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Example adapted by: Charles Fernandes de Souza
+  Date: December 18, 2024
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService {
+
+    List<Object> list = new ArrayList<>();
+
+    public void addValue(Object value) {
+        list.add(value);
+    }
+
+    public Object first() {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("List is empty!");
+        }
+        return list.getFirst();
+    }
+
+    public void print() {
+        System.out.print("[");
+        if (!list.isEmpty()) {
+            System.out.print(list.getFirst());
+        }
+        for (int i = 1; i < list.size(); i++) {
+            System.out.print(", " + list.get(i));
+        }
+        System.out.print("]");
+    }
+}
