02d0e00203970dab288b3c31ae6fceb9c11c145e
souzafcharles <souzach@outlook.com>
2024-12-23 17:18:46 -0300

    Solution | Example 05 | Section Generics, Set and Map

--- Section_O15_Generics_Set_and_Map/ProblemStatementExample05/src/com/souza/charles/view/application/App.java
+++ Section_O15_Generics_Set_and_Map/ProblemStatementExample05/src/com/souza/charles/view/application/App.java
@@ -1,8 +1,35 @@
 package com.souza.charles.view.application;
 
+import com.souza.charles.model.entities.Product;
+import com.souza.charles.model.services.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
 
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\tmp\\in.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = bufferedReader.readLine();
+            }
+            Product product = CalculationService.max(list);
+            System.out.println("Most expensive:");
+            System.out.println(product);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
