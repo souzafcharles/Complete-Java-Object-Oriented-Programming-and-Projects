aa94136b500a29830bc1f6682a5d288c45867ef7
souzafcharles <souzach@outlook.com>
2024-12-27 14:25:36 -0300

    Project set up | Exercise 02 | Section Generics, Set and Map

--- Section_O15_Generics_Set_and_Map/ProblemStatementExercise02/src/com/souza/charles/view/application/Main.java
+++ Section_O15_Generics_Set_and_Map/ProblemStatementExercise02/src/com/souza/charles/view/application/Main.java
@@ -1,6 +1,38 @@
 package com.souza.charles.view.application;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Integer> A = new HashSet<>();
+        Set<Integer> B = new HashSet<>();
+        Set<Integer> C = new HashSet<>();
+        System.out.print("How many students for course A? ");
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int uniqueCode = scanner.nextInt();
+            A.add(uniqueCode);
+        }
+        System.out.print("How many students for course B? ");
+        N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int uniqueCode = scanner.nextInt();
+            B.add(uniqueCode);
+        }
+        System.out.print("How many students for course C? ");
+        N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int uniqueCode = scanner.nextInt();
+            C.add(uniqueCode);
+        }
+        Set<Integer> total = new HashSet<>(A);
+        total.addAll(B);
+        total.addAll(C);
+        System.out.println("Total students: " + total.size());
+        scanner.close();
     }
 }
+
