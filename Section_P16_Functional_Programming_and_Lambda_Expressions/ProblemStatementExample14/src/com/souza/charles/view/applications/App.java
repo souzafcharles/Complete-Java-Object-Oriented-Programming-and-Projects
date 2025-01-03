8cbd4553d8e665043bb377adc782ec65c7b3ea4b
souzafcharles <souzach@outlook.com>
2025-01-03 18:54:37 -0300

    Solution | Example 14 | Section Functional Programming and Lambda Expressions

---
+++ Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample14/src/com/souza/charles/view/applications/App.java
@@ -0,0 +1,30 @@
+package com.souza.charles.view.applications;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Example adapted by: Charles Fernandes de Souza
+  Date: January 03, 2025
+ */
+
+import com.souza.charles.model.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class App {
+    public static void main(String[] args) {
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("TV", 900.00));
+        productList.add(new Product("Mouse", 50.00));
+        productList.add(new Product("Tablet", 450.00));
+        productList.add(new Product("HD Case", 80.90));
+
+        Consumer<Product> consumer = product -> product.setPrice(product.getPrice() * 1.1);
+        productList.forEach(consumer);
+
+        for (Product product : productList) {
+            System.out.println(product);
+        }
+    }
+}
