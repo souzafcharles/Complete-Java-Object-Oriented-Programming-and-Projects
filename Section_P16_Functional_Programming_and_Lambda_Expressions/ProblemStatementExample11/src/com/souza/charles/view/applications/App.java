933c44cc9d1eea51bb6a536554639c18a129d21d
souzafcharles <souzach@outlook.com>
2025-01-03 19:14:08 -0300

    Solution | Example 11 | Section Functional Programming and Lambda Expressions

--- Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample11/src/com/souza/charles/view/applications/App.java
+++ Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample11/src/com/souza/charles/view/applications/App.java
@@ -20,9 +20,6 @@ public class App {
         productList.add(new Product("HD Case", 80.90));
 
         productList.forEach(new ProductConsumer());
-        
-        for (Product product : productList) {
-            System.out.println(product);
-        }
+     	productList.forEach(System.out::println);
     }
 }
