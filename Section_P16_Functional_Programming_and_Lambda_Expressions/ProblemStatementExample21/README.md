b68702903c7e017c5ddcc475021a7ac3ad9b40b6
souzafcharles <souzach@outlook.com>
2025-01-06 15:11:38 -0300

    Project set up | Example 21 | Section Functional Programming and Lambda Expressions

---
+++ Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample21/README.md
@@ -0,0 +1,23 @@
+# Problem Statement Example 21 - Passing Functions as Arguments to other Functions.
+
+**Instructions**: Assume a Product class with name and price attributes. From a list of products, calculate the
+sum of the prices of only those products whose name begins with “T”.
+
+### Product Model
+
+![Product Model](https://github.com/souzafcharles/Complete-Java-Object-Oriented-Programming-and-Projects/blob/master/Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample21/product-model.png)
+
+### Example Inputs and Outputs:
+
+| **Input**          | **Output** |
+|--------------------|------------|
+| `TV`, `900.00`     | $1250.50   |
+| `Mouse`, `50.00`   |            |
+| `Tablet`, `350.00` |            |
+| `HD Case`, `80.90` |            |
+
+### Functions as Arguments
+
+```java
+
+```
---
+++ Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample21/src/com/souza/charles/model/entities/Product.java
@@ -0,0 +1,41 @@
+package com.souza.charles.model.entities;
+/*
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Example adapted by: Charles Fernandes de Souza
+ Date: January 06, 2025
+*/
+public class Product {
+
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - $%.2f", name, price);
+    }
+}
