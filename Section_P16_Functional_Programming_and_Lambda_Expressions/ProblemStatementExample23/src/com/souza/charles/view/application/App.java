11008de67758c81af152917ae08b3ff1d748d4f8
souzafcharles <souzach@outlook.com>
2025-01-06 22:00:05 -0300

    Project set up | Example 23 | Section Functional Programming and Lambda Expressions

---
+++ Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample23/src/com/souza/charles/view/application/App.java
@@ -0,0 +1,13 @@
+package com.souza.charles.view.application;
+/*
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Example adapted by: Charles Fernandes de Souza
+ Date: January 06, 2025
+*/
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
