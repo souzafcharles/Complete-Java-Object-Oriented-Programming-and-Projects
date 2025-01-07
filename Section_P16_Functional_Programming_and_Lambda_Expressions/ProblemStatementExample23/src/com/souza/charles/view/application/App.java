fbdce7efce88e7fc9929db45564cb59202f591db
souzafcharles <souzach@outlook.com>
2025-01-06 22:14:00 -0300

    Solution | Example 23 | Section Functional Programming and Lambda Expressions

--- Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample23/src/com/souza/charles/view/application/App.java
+++ Section_P16_Functional_Programming_and_Lambda_Expressions/ProblemStatementExample23/src/com/souza/charles/view/application/App.java
@@ -6,8 +6,25 @@ package com.souza.charles.view.application;
  Date: January 06, 2025
 */
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
     public static void main(String[] args) {
 
+        List<Integer> list = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Stream<Integer> stream01 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(stream01.toArray()));
+
+        Integer sum = list.stream().reduce(0, (x, y) -> x + y);
+        System.out.println("Sum = " + sum);
+
+        List<Integer> stream03 = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .collect(Collectors.toList());
+        System.out.println(Arrays.toString(stream03.toArray()));
     }
 }
