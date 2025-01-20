11c7615bf64d240bafbf2df32148344c3083cb4b
souzafcharles <souzach@outlook.com>
2025-01-20 18:15:23 -0300

    Project set up | Section Project: Web Services with Spring Boot and JPA/Hibernate

---
+++ Section_S19_Project_Web_Services_with_Spring_Boot_and_JPA_Hibernate/webServices/src/main/java/com/souza/charles/webServices/WebServicesApplication.java
@@ -0,0 +1,19 @@
+package com.souza.charles.webServices;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Project done by: Charles Fernandes de Souza
+  Date: January 20, 2025
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesApplication.class, args);
+    }
+
+}
