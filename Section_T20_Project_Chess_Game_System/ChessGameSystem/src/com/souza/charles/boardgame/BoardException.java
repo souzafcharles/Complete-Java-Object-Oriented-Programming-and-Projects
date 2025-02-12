1ae019fd4e1c923528d796a46e5d467b5c91223b
souzafcharles <souzach@outlook.com>
2025-02-12 15:25:41 -0300

    BoardException and defensive programming | Chess Game System

---
+++ Section_T20_Project_Chess_Game_System/ChessGameSystem/src/com/souza/charles/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+package com.souza.charles.boardgame;
+/*
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Project done by: Charles Fernandes de Souza
+ Date: February 12, 2025
+*/
+
+public class BoardException extends RuntimeException {
+    public BoardException(String message) {
+        super(message);
+    }
+}
