948cb01f51387c929b66c2dd8bfbbe920b06a625
souzafcharles <souzach@outlook.com>
2025-02-12 15:55:36 -0300

    ChessException and ChessPosition | Chess Game System

---
+++ Section_T20_Project_Chess_Game_System/ChessGameSystem/src/com/souza/charles/chessgame/ChessException.java
@@ -0,0 +1,13 @@
+package com.souza.charles.chessgame;
+/*
+ Course title: Complete Java - Object-Oriented Programming + Projects
+ Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+ Project done by: Charles Fernandes de Souza
+ Date: February 12, 2025
+*/
+
+public class ChessException extends RuntimeException {
+    public ChessException(String message) {
+        super(message);
+    }
+}
---
+++ Section_T20_Project_Chess_Game_System/ChessGameSystem/src/com/souza/charles/chessgame/ChessPosition.java
@@ -0,0 +1,46 @@
+package com.souza.charles.chessgame;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Project done by: Charles Fernandes de Souza
+  Date: February 12, 2025
+ */
+
+import com.souza.charles.boardgame.Position;
+
+import java.io.Serializable;
+
+public class ChessPosition implements Serializable {
+
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
