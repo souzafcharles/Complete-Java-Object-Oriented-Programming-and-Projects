25658155758cfd35e5a92596f6462f17546171b3
souzafcharles <souzach@outlook.com>
2025-02-14 20:17:05 -0300

    Project set up | Graphical Interface with JavaFX

---
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/view/Application.java
@@ -0,0 +1,28 @@
+package com.souza.charles.graphicalapp.view;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Project done by: Charles Fernandes de Souza
+  Date: February 14, 2025
+ */
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/com/souza/charles/graphicalapp/application-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
