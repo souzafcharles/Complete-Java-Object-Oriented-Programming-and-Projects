869431d7370fe440414ae344afd48b9268cc3a92
souzafcharles <souzach@outlook.com>
2025-02-15 09:02:12 -0300

    Show Alert | Graphical Interface with JavaFX

--- Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/controller/ViewController.java
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/controller/ViewController.java
@@ -1,6 +1,14 @@
 package com.souza.charles.graphicalapp.controller;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Project done by: Charles Fernandes de Souza
+  Date: February 14, 2025
+ */
 
+import com.souza.charles.graphicalapp.view.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -10,6 +18,9 @@ public class ViewController {
 
     @FXML
     private void onBtTestAction() {
-        System.out.println("Welcome!");
+        Alerts.showAlert("System Notification", "Scheduled Maintenance",
+                "We apologize for any inconvenience.",
+                Alert.AlertType.INFORMATION);
     }
+
 }
---
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/view/util/Alerts.java
@@ -0,0 +1,20 @@
+package com.souza.charles.graphicalapp.view.util;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Project done by: Charles Fernandes de Souza
+  Date: February 14, 2025
+ */
+
+import javafx.scene.control.Alert;
+
+public class Alerts {
+
+    public static void showAlert(String title, String header, String content, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.show();
+    }
+}
