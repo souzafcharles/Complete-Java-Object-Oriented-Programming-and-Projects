e7a1bb2496325f29788d9a2b693dc861d056f15f
souzafcharles <souzach@outlook.com>
2025-02-15 09:31:08 -0300

    Enhanced User Interface for Sum Calculation Application | Graphical Interface with JavaFX

--- Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/controller/ViewController.java
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/controller/ViewController.java
@@ -10,17 +10,35 @@ import com.souza.charles.graphicalapp.view.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class ViewController {
 
     @FXML
-    private Button btTest;
+    private TextField txtNumber1;
 
     @FXML
-    private void onBtTestAction() {
-        Alerts.showAlert("System Notification", "Scheduled Maintenance",
-                "We apologize for any inconvenience.",
-                Alert.AlertType.INFORMATION);
-    }
+    private TextField txtNumber2;
 
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    public void onBtSumClick() {
+        try {
+            Locale.setDefault(Locale.UK);
+            double height = Double.parseDouble(txtNumber1.getText());
+            double width = Double.parseDouble(txtNumber2.getText());
+            double sum = height + width;
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Input Error", "Invalid Input", "Please enter valid numbers in both fields.", Alert.AlertType.ERROR);
+        }
+    }
 }
