a517d7b729008f826bcb5a813dffed823a557b03
souzafcharles <souzach@outlook.com>
2025-02-14 21:20:22 -0300

    Add initial FXML setup | Graphical Interface with JavaFX

--- Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/view/Main.java
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/view/Main.java
@@ -7,19 +7,26 @@ package com.souza.charles.graphicalapp.view;
  */
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+
+
+public class Main extends javafx.application.Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/com/souza/charles/graphicalapp/application-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/com/souza/charles/graphicalapp/View.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/module-info.java
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-01/GraphicalApp/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.souza.charles.graphicalapp {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.souza.charles.graphicalapp.controller to javafx.fxml;
     opens com.souza.charles.graphicalapp.view to javafx.fxml;
 
     exports com.souza.charles.graphicalapp.view;
