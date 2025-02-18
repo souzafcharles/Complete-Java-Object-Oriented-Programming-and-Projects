4196c3f2ab427de1030ab95d4e4b13dd38015b1e
souzafcharles <souzach@outlook.com>
2025-02-17 22:18:39 -0300

    Refactor: Restructure Section A1 Introduction to the Java Language

---
+++ Section_A1_Introduction_to_the_Java_Language/Example01/src/src/souza/charles/Example_01.java
@@ -0,0 +1,13 @@
+package src.souza.charles;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Example adapted by: Charles Fernandes de Souza
+  Date: February 17, 2025
+ */
+
+public class Example_01 {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
