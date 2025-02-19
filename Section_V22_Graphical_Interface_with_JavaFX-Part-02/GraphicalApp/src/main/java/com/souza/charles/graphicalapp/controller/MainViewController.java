ac09fed1a012d00d1319f5626a1830c13f11423e
souzafcharles <souzach@outlook.com>
2025-02-19 16:02:38 -0300

    JavaFX UI for Main View Controller | Graphical Interface with JavaFX

---
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-02/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/controller/MainViewController.java
@@ -0,0 +1,45 @@
+package com.souza.charles.graphicalapp.controller;
+ /*
+  Course title: Complete Java - Object-Oriented Programming + Projects
+  Instructor: Prof. Dr. Nelio Alves - Udemy, Inc.
+  Project done by: Charles Fernandes de Souza
+  Date: February 19, 2025
+ */
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("01");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("02");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("03");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
--- Section_V22_Graphical_Interface_with_JavaFX-Part-02/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/view/Main.java
+++ Section_V22_Graphical_Interface_with_JavaFX-Part-02/GraphicalApp/src/main/java/com/souza/charles/graphicalapp/view/Main.java
@@ -7,8 +7,8 @@ package com.souza.charles.graphicalapp.view;
  */
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,9 +17,11 @@ public class Main extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/souza/charles/graphicalapp/View.fxml"));
-            Parent parent = loader.load();
-            Scene mainScene = new Scene(parent);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/souza/charles/graphicalapp/MainView.fxml"));
+            ScrollPane scrollPane = loader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX Application");
             primaryStage.show();
